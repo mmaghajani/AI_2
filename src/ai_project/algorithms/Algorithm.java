a3c9c00e1306f87d39697b0d0fb47479d979e1b0
mahdi aghajani <mma137421@gmail.com>
2016-12-09 21:30:10 +0330

    Corrects algorithm classes

--- src/ai_project/algorithms/Algorithm.java
+++ src/ai_project/algorithms/Algorithm.java
@@ -10,29 +10,10 @@ import java.util.ArrayList;
  * Created by mma on 10/26/16.
  */
 public abstract class Algorithm {
-    protected ArrayList<Node> f;
-    protected ArrayList<Node> e;
-    protected int numOfExpandedNode = 0;
-    protected int numOfVisitedNode = 0;
+
     protected int maxUsedMemory = 0;
 
     public abstract Node apply(Problem problem);
-
-    public void increaseNumOfExpandedNode() {
-        this.numOfExpandedNode++;
-    }
-
-    public void setNumOfExpandedNode(int number) {
-        this.numOfExpandedNode = number;
-    }
-
-    public void increaseNumOfVisitedNode() {
-        this.numOfVisitedNode++;
-    }
-
-    public void setNumOfVisitedNode(int number) {
-        this.numOfVisitedNode = number;
-    }
 
     public void setMaxUsedMemory(int count) {
         maxUsedMemory = count;
@@ -40,13 +21,5 @@ public abstract class Algorithm {
 
     public int getMaxUsedMemory() {
         return maxUsedMemory;
-    }
-
-    public int getNumOfExpandedNode() {
-        return numOfExpandedNode;
-    }
-
-    public int getNumOfVisitedNode() {
-        return numOfVisitedNode;
     }
 }
