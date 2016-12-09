29502b83773fa2faa5d37cb246e309737acbd2fe
mahdi aghajani <mma137421@gmail.com>
2016-12-09 21:29:18 +0330

    Corrects algorithm classes

--- src/ai_project/algorithms/individual/IndividualAlgorithm.java
+++ src/ai_project/algorithms/individual/IndividualAlgorithm.java
@@ -6,4 +6,32 @@ import ai_project.algorithms.Algorithm;
  * Created by mma on 12/9/16.
  */
 public abstract class IndividualAlgorithm extends Algorithm {
+    protected int numOfExpandedNode = 0;
+
+    protected int numOfVisitedNode = 0;
+
+    public void increaseNumOfExpandedNode() {
+        this.numOfExpandedNode++;
+    }
+
+    public void setNumOfExpandedNode(int number) {
+        this.numOfExpandedNode = number;
+    }
+
+    public void increaseNumOfVisitedNode() {
+        this.numOfVisitedNode++;
+    }
+
+    public void setNumOfVisitedNode(int number) {
+        this.numOfVisitedNode = number;
+    }
+
+    public int getNumOfExpandedNode() {
+        return numOfExpandedNode;
+    }
+
+    public int getNumOfVisitedNode() {
+        return numOfVisitedNode;
+    }
+
 }
--- src/ai_project/algorithms/population/PopulationAlgorithm.java
+++ src/ai_project/algorithms/population/PopulationAlgorithm.java
@@ -2,8 +2,12 @@ package ai_project.algorithms.population;
 
 import ai_project.algorithms.Algorithm;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by mma on 12/9/16.
  */
 public abstract class PopulationAlgorithm extends Algorithm {
+    protected HashMap<Integer,ArrayList<Double>> details ;
 }
