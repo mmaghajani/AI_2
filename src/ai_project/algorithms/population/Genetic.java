a4880f8746b54c0bad16a0fcb8818055d1e4314e
mahdi aghajani <mma137421@gmail.com>
2016-12-09 21:37:38 +0330

    Crrects GA

--- src/ai_project/algorithms/population/Genetic.java
+++ src/ai_project/algorithms/population/Genetic.java
@@ -6,6 +6,7 @@ import ai_project.data_structures.Node;
 import ai_project.problems.Problem;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by mma on 12/8/16.
@@ -15,34 +16,64 @@ public class Genetic extends PopulationAlgorithm {
     private int N = Constants.NUMBER_OF_POPULATION;
     private int K = Constants.PARENT_SELECTION_RATE;
 
+    public Genetic() {
+        details = new HashMap<>();
+    }
+
     @Override
     public Node apply(Problem problem) {
         population = generateInitialPopulation(problem);
         int stepLimit = Constants.GA_STEP_LIMITATION;
         while (stepLimit > 0) {
+            details.put(Constants.GA_STEP_LIMITATION - stepLimit, computeDetails(problem));
             ArrayList<Node> parents = selectParentsWithTournamentSelection(problem);
             ArrayList<Node> children = crossoverAndOffspring(parents, problem);
             children = mutation(children, problem);
-            population = remainingSelection(children , problem);
+            population = remainingSelection(children, problem);
             stepLimit--;
         }
-        return bestNode(population , problem);
+        return bestNode(population, problem);
     }
 
-    private Node bestNode(ArrayList<Node> population , Problem problem) {
+    private ArrayList<Double> computeDetails(Problem problem) {
+        population.sort((node, t1) -> {
+            if (problem.objectiveFunction(node) > problem.objectiveFunction(t1))
+                return 1;
+            else if (problem.objectiveFunction(node) == problem.objectiveFunction(t1))
+                return 0;
+            else
+                return -1;
+        });
+
+        int sum = 0;
+        double avg;
+        for (int i = 0; i < population.size(); i++)
+            sum += problem.objectiveFunction(population.get(i));
+        avg = sum / population.size();
+        //result array that first member's is best fitness and last member's
+        //is worth fitness and middle is average
+        ArrayList<Double> result = new ArrayList<>();
+        result.add(problem.objectiveFunction(population.get(population.size() - 1)));
+        result.add(problem.objectiveFunction(population.get(0)));
+        result.add(avg);
+
+        return result;
+    }
+
+    private Node bestNode(ArrayList<Node> population, Problem problem) {
         Node max = population.get(0);
-        for( int i = 0 ; i < population.size() ; i++ ){
-            if( problem.objectiveFunction(population.get(i)) > problem.objectiveFunction(max)){
+        for (int i = 0; i < population.size(); i++) {
+            if (problem.objectiveFunction(population.get(i)) > problem.objectiveFunction(max)) {
                 max = population.get(i);
             }
         }
         return max;
     }
 
-    private ArrayList<Node> remainingSelection(ArrayList<Node> children , Problem problem) {
+    private ArrayList<Node> remainingSelection(ArrayList<Node> children, Problem problem) {
         MathHandler math = MathHandler.getInstance();
         //Adds K/2 node to population
-        for( int i = 0 ; i < children.size() ; i++ ){
+        for (int i = 0; i < children.size(); i++) {
             population.add(children.get(i));
         }
         //Deletes K/2 node from population based on fitness
@@ -54,9 +85,9 @@ public class Genetic extends PopulationAlgorithm {
             else
                 return -1;
         });
-        for (int i = 0; i < K/2; i++) {
-            int x  = math.getIntegerRandNum(population.size());
-            population.remove(population.size() - x ) ;
+        for (int i = 0; i < K / 2; i++) {
+            int x = math.getIntegerRandNum(population.size());
+            population.remove(population.size() - x);
         }
 
         return population;
