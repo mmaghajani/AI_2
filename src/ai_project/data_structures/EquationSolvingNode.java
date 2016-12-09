5c7b3482f85286be5129ac80ea956fc6b85335b9
mahdi aghajani <mma137421@gmail.com>
2016-12-09 13:27:12 +0330

    Implements equation solving node

--- src/ai_project/data_structures/EquationSolvingNode.java
+++ src/ai_project/data_structures/EquationSolvingNode.java
@@ -4,8 +4,31 @@ package ai_project.data_structures;
  * Created by mma on 12/9/16.
  */
 public class EquationSolvingNode extends Node {
+    private double state = 0.2;
+
+    public EquationSolvingNode(int state) {
+        this.state = state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if ((int) o == state)
+            return true;
+        else
+            return false;
+    }
+
+    @Override
+    public String toString() {
+        return "sin (" + state + ") ? " + state + "^2 - " + state ;
+    }
+
     @Override
     public Object getState() {
-        return null;
+        return state ;
     }
 }
